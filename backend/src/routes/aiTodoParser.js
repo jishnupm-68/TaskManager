6ea9f7cd66aacc4cfc30465a5ac75b53f1ced4ed
Jishnu PM <jishnupmbcr68@gail.com>
2025-12-05 17:33:14 +0530

    bugfix and cleanup

--- backend/src/routes/aiTodoParser.js
+++ backend/src/routes/aiTodoParser.js
@@ -3,21 +3,22 @@ const aiTodoParserRouter = express.Router();
 const { GoogleGenAI } = require('@google/genai');
 
 
-aiTodoParserRouter.post('/todoParser',async(req,res)=>{
-try {
-        const ai = new GoogleGenAI({apiKey: process.env.GEMINI_API_KEY});
-        const {text} = req.body;
-        
-        const currentDate = new Date();
-        console.log("text" , text, currentDate)
-        const prompt = `
+aiTodoParserRouter.post("/todoParser", async (req, res) => {
+  try {
+    const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
+    const { text } = req.body;
+
+    const currentDate = new Date();
+    if (text.length <= 0) return;
+
+    const prompt = `
                 You are a task parser. Convert the following text into a JSON object with keys: 
                 "title", "priority", "status", "dueDate".
 
                 Rules:
                 - Always respond in valid JSON only.
-                - "priority" should be one of (low, medium, high).
-                - "status" should be one of (pending, in-progress, completed).
+                - "priority" should be one of (Low, Medium, High).
+                - "status" should be one of (Todo, In Progress, Completed).
                 - "dueDate" must be in YYYY-MM-DD HH-MM format. If no date mentioned, return null.
                 - Extract only meaningful title without extra words.
                 - Current Date is: "${currentDate}", due date must be based on this date.
@@ -26,27 +27,21 @@ try {
 
                 Text: "${text}"
                 `;
-  const response = await ai.models.generateContent({
-    model: 'gemini-2.5-flash-lite',
-    contents: prompt,
-  });
-  console.log(response.text);
-
-  res.status(200).json({status:true, message:"Text parsed successfully", data:JSON.parse(response.text)})
-
-
-
-} catch (error) {
+    const response = await ai.models.generateContent({
+      model: "gemini-2.5-flash-lite",
+      contents: prompt,
+    });
+    res
+      .status(200)
+      .json({
+        status: true,
+        message: "Text parsed successfully",
+        data: JSON.parse(response.text),
+      });
+  } catch (error) {
     console.log(error.message);
-    res.status(500).json({status:false, message:error.message});
-}
-})
-
-
-
-
-
-// Uses the GEMINI_API_KEY environment variable if apiKey not specified
-
+    res.status(500).json({ status: false, message: error.message });
+  }
+});
 
 module.exports= aiTodoParserRouter
